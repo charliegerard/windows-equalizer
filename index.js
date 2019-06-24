cdf8a2a24f1c9b8f8bc12a3269b73e5ca8726728
Charlie Gerard <annecharlotte.gerard@gmail.com>
2019-06-24 19:30:21 +1000

    going back to less windows

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ let windowOne, windowTwo, windowThree, windowFour, windowFive, windowSix;
 const numWindows = Math.floor(resolutionHeight / windowMinHeight);
 
 let xCoordinateRightWindow = screen.availWidth - 163;
+let colors = ['red', 'green', 'blue', 'white', 'grey', 'pink'];
 
 let analyser, freqs, bufferLength, dataArray;
-// let allWindows = [];
 let allLeftWindows = [];
-let allRightWindows = [];
 let screenY, screenX;
 let base;
 
@@ -19,33 +18,19 @@ window.onload = () => {
     if(window.name === ""){
         base = window;
 
-        button.onclick = () => {
-            // windowOne = window.open("windows/index1.html", "windowOne", "location=1,status=1,scrollbars=1,width=100,height=100,screenX=0,screenY=0"); 
-            // windowTwo = window.open("windows/index2.html", "windowTwo", `location=1,status=1,scrollbars=1,width=100,height=100,screenX=${screen.availWidth - 100},screenY=0`); 
-            // windowThree = window.open("windows/index3.html", "windowThree", `location=1,status=1,scrollbars=1,width=100,height=100,screenX=0,screenY=${windowTwo.screenY + windowMinHeight}`); 
-            // windowFour = window.open("windows/index4.html", "windowFour", `location=1,status=1,scrollbars=1,width=100,height=100,screenX=0,screenY=${windowThree.screenY + windowMinHeight}`); 
-            // windowFive = window.open("windows/index5.html", "windowFive", `location=1,status=1,scrollbars=1,width=100,height=100,screenX=0,screenY=${windowFour.screenY + windowMinHeight}`); 
-            // windowSix = window.open("windows/index6.html", "windowSix", `location=1,status=1,scrollbars=1,width=100,height=100,screenX=0,screenY=${windowFive.screenY + windowMinHeight}`); 
-    
+        button.onclick = () => {    
             for(var i = 0; i < numWindows; i++){
                 allLeftWindows.push(i);
-                allRightWindows.push(i);
-
                 screenY = (i === 0) ? 0 : allLeftWindows[i-1].screenY + windowMinHeight;
-    
                 allLeftWindows[i] = window.open("windows/index1.html", `window-left${i}`, `location=1,status=1,scrollbars=1,width=100,height=100,screenX=0,screenY=${screenY}`);
-                allRightWindows[i] = window.open("windows/index1.html", `window-right${i}`, `location=1,status=1,scrollbars=1,width=100,height=100,screenX=${xCoordinateRightWindow},screenY=${screenY}`);
             }
-    
+            setColors();
             startSound();
         }
     } else {
         base = window.opener;
         for(var i = 0; i < allLeftWindows.length; i++){
             allLeftWindows[i] = base.allLeftWindows[i];
-        }
-        for(var i = 0; i < allRightWindows.length; i++){
-            allRightWindows[i] = base.allRightWindows[i];
         }
     }   
     
@@ -60,14 +45,16 @@ window.onload = () => {
     })
 } 
 
+const setColors = () => {
+    for(var i = 0; i < allLeftWindows.length; i++){
+        allLeftWindows[i].document.body.style.backgroundColor = 'pink';
+    }
+}
 const quit = () => {
     for(var i = 0; i < allLeftWindows.length; i++){
         allLeftWindows[i].close()
     }
-
-    for(var i = 0; i < allRightWindows.length; i++){
-        allRightWindows[i].close()
-    }
+    window.cancelAnimationFrame(draw);
 }
 
 const startSound = () => {
@@ -119,16 +106,6 @@ const startSound = () => {
         for(var i = 0; i < numWindows; i++){
             width = dataArray[i] * 1.5;
             allLeftWindows[i].resizeTo(width, allLeftWindows[i].outerHeight);
-
-            let screenY2 = (i === 0) ? 0 : allRightWindows[i-1].screenY + windowMinHeight;
-
-            if(width < xCoordinateRightWindow){
-                allRightWindows[i].moveTo(screen.availWidth - width, screenY2);
-                allRightWindows[i].resizeTo(width, allRightWindows[i].outerHeight);
-            } else {
-                allRightWindows[i].moveTo(xCoordinateRightWindow, screenY2);
-                allRightWindows[i].resizeTo(width, allRightWindows[i].outerHeight);
-            }
         }
     }
 }
